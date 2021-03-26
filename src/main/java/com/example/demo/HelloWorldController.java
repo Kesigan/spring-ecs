92ccc28a9aa2b3aede99b700409dff0b939e10ed
Kesigan <kesigan@gmail.com>
2021-03-26 22:19:03 +0530

    fixed

--- src/main/java/com/example/demo/HelloWorldController.java
+++ src/main/java/com/example/demo/HelloWorldController.java
@@ -8,10 +8,6 @@ public class HelloWorldController {
  
     @GetMapping("/hello")
     public String sayHello() {
-<<<<<<< HEAD
         return "Hello world spring boot ECS!";
-=======
-        return "Hello world Kesigan!";
->>>>>>> c005b8a06da7996ee468280bb5e16f4a039f10bf
     }
 }
